ce500dbbf783f87c3801b351c1b918492dfe4118
janfajessen <janfajessen@gmail.com>
2019-03-21 09:53:29 +0100

    decremente, reset, and bg color change

--- backend/index.js
+++ backend/index.js
@@ -1,74 +1,142 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
 
-const fs = require( 'fs' );
+const fs = require('fs');
 
-const port =  4000;
+const port = 4000;
 
 
 // BODY PARSE TO JSON
-app.use( express.json() );
+app.use(express.json());
 
 
 // enable CORS
-app.use( function ( req, res, next ) {
-    res.header( "Access-Control-Allow-Origin", "*" );
-    res.header( "Access-Control-Allow-Methods", "GET, PUT, POST, DELETE" );
-    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-} );
+});
 
 
 
-app.get('/data', (req,res) => {
+app.get('/data', (req, res) => {
 
-    const jsonString =  fs.readFileSync('./db.json','UTF-8');
+    const jsonString = fs.readFileSync('./db.json', 'UTF-8');
     const data = JSON.parse(jsonString);
     res.json(data);
 
 })
 
-app.get( '/increment', ( req, res ) => {
+app.get('/increment', (req, res) => {
 
-     // READ JSON FROM ROM
-    const jsonString = fs.readFileSync( './db.json', 'UTF-8' );
-    const data = JSON.parse( jsonString );
+    // READ JSON FROM ROM
+    const jsonString = fs.readFileSync('./db.json', 'UTF-8');
+    const data = JSON.parse(jsonString);
 
 
     // UPDATE DATA ON RAM
     data.counterValue += 1;
 
     // WRITE BACK THE DATA TO ROM
-    fs.writeFileSync( './db.json' , JSON.stringify( data))
+    fs.writeFileSync('./db.json', JSON.stringify(data))
 
-    res.json( data );
+    res.json(data);
 
-} )
+})
 
-app.get( '/incrementBy/:amount', ( req, res ) => {
+app.get('/decrement', (req, res) => {
+
+    //READ JSON FROM ROM
+    const jsonString = fs.readFileSync('./db.json', 'UTF-8');
+    const data = JSON.parse(jsonString);
+
+
+    //UPDATE DATA ON RAM
+    data.counterValue -= 1;
+
+    //WRITE BACK THE DATA TO ROM
+    fs.writeFileSync('./db.json', JSON.stringify(data))
+
+    res.json(data);
+
+})
+
+app.get('/reset', (req, res) => {
+
+    //READ JSON FROM ROM
+    const jsonString = fs.readFileSync('./db.json', 'UTF-8');
+    const data = JSON.parse(jsonString);
+
+
+    //UPDATE DATA ON RAM
+    data.counterValue = 0;
+
+    //WRITE BACK THE DATA TO ROM
+    fs.writeFileSync('./db.json', JSON.stringify(data))
+
+    res.json(data);
+
+})
+
+app.get('/decrement', (req, res) => {
+
+    //READ JSON FROM ROM
+    const jsonString = fs.readFileSync('./db.json', 'UTF-8');
+    const data = JSON.parse(jsonString);
+
+
+    //UPDATE DATA ON RAM
+    data.counterValue -= 1;
+
+    //WRITE BACK THE DATA TO ROM
+    fs.writeFileSync('./db.json', JSON.stringify(data))
+
+    res.json(data);
+
+})
+
+app.get('/picker/:color', (req, res) => {
+
+
+    const jsonString = fs.readFileSync('./db.json', 'UTF-8');
+    const data = JSON.parse(jsonString);
+
+
+
+    data.color = req.params.color;
+
+
+    fs.writeFileSync('./db.json', JSON.stringify(data))
+
+    res.json(data);
+
+})
+
+app.get('/incrementBy/:amount', (req, res) => {
 
     // READ JSON FROM ROM
-    const jsonString = fs.readFileSync( './db.json', 'UTF-8' );
-    const data = JSON.parse( jsonString );
+    const jsonString = fs.readFileSync('./db.json', 'UTF-8');
+    const data = JSON.parse(jsonString);
 
 
-    let amount = Number( req.params.amount )
+    let amount = Number(req.params.amount)
     if (isNaN(amount)) {
-        res.status(500).json({message:'UPS :('})
+        res.status(500).json({ message: 'UPS :(' })
         return;
     }
     // UPDATE DATA ON RAM
     data.counterValue += amount;
 
     // WRITE BACK THE DATA TO ROM
-    fs.writeFileSync( './db.json', JSON.stringify( data ) );
+    fs.writeFileSync('./db.json', JSON.stringify(data));
 
-    res.json( data );
+    res.json(data);
 
-} )
-
-
+})
 
 
 
-app.listen( port, () => console.log( 'Servidor levantado en ' + port ) );
+
+
+app.listen(port, () => console.log('Servidor levantado en ' + port));
--- frontend/main.js
+++ frontend/main.js
@@ -1,36 +1,95 @@
-document.addEventListener( 'DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
 
     // declarations
     ///////////////
     const baseApiUrl = 'http://localhost:4000';
-    const counterNode = document.querySelector( '.counter' );
+    const counterNode = document.querySelector('.counter');
 
     const updateCounterDOM = (value) => {
-        counterNode.innerText = String( value )
+        counterNode.innerText = String(value)
     }
 
     // get initial value from backend
-    fetch( baseApiUrl + '/data' )
-        .then( response => response.json() )
-        .then( data => {
-            updateCounterDOM( data.counterValue )
-        } )
+    fetch(baseApiUrl + '/data')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            updateCounterDOM(data.counterValue)
+        })
 
     // Listeners
     ////////////
 
-    // listener for incremente
-    document.querySelector( 'header .increment' ).addEventListener( 'click', () => {
+    // listener for increment
+    document.querySelector('header .increment').addEventListener('click', () => {
         // increment counter
 
 
-        fetch( baseApiUrl  +'/increment' )
-            .then (res => res.json())
-            .then( data => {
+        fetch(baseApiUrl + '/increment')
+            .then(res => res.json())
+            .then(data => {
                 updateCounterDOM(data.counterValue)
             })
 
-    } )
+    })
+    //listener for decrement
+    document.querySelector('header .decrement').addEventListener('click', () => {
+        // decrement counter
+
+
+        fetch(baseApiUrl + '/decrement')
+            .then(res => res.json())
+            .then(data => {
+                updateCounterDOM(data.counterValue)
+            })
+
+    })
+
+    //listener for reset
+    document.querySelector('header .reset').addEventListener('click', () => {
+        // decrement counter to 0
+
+
+        fetch(baseApiUrl + '/reset')
+            .then(res => res.json())
+            .then(data => {
+                updateCounterDOM(data.counterValue)
+            })
+
+    })
+
+    //listener for color
+    document.querySelector('header .picker').addEventListener('change', (ev) => {
+        // console.log(ev.target.value)
+        // change color
+        let newColor = encodeURIComponent(ev.target.value);
+        console.log(newColor);
+
+        let endpoint = baseApiUrl + '/picker/' + newColor;
+        console.log(endpoint);
+
+        fetch(endpoint)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                let contrast = getContrastRatio('#fff', data.color);
+                console.log(contrast);
+                let doc = document.querySelector('main');
+                doc.style.backgroundColor = data.color;
+
+                if (contrast > 2) {
+                    doc.style.color = "#fff";
+
+                } else {
+                    doc.style.color = "#000";
+
+                }
+
+
+            })
+
+    })
+
 
     // listener for increment by
     document.querySelector('header .incrementBy').addEventListener('keyup', (ev) => {
@@ -38,15 +97,15 @@ document.addEventListener( 'DOMContentLoaded', function () {
         if (ev.keyCode === 13) {
             let amount = ev.target.value;
 
-            fetch( baseApiUrl + '/incrementBy/' + amount )
-                .then( res => res.json() )
-                .then( data => {
-                    updateCounterDOM( data.counterValue )
+            fetch(baseApiUrl + '/incrementBy/' + amount)
+                .then(res => res.json())
+                .then(data => {
+                    updateCounterDOM(data.counterValue)
                     ev.target.value = '';
-                } )
+                })
                 .catch(console.error)
 
 
         }
     })
-} )
+})
